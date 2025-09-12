5b0a2affb3a5199f5f512a8106f0971de440db7c
sulakshana kalugamge <sulakshana.asus@gmail.com>
2025-09-12 07:02:22 +0530

    Final done for good

--- pages/api/products/[slug].ts
+++ pages/api/products/[slug].ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { dbConnect } from '@/lib/db'
 import Product from '@/models/Product'
